57627db0570df486f342965eb24cbf164dff9187
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-06-19 11:23:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let state= document.querySelector("#status");
+let btn=document.querySelector("#enterBtn");
+btn.addEventListener("click",()=>{
+	state.innerHTML = "Entered Metaverse";
+});
+console.log("hello");
